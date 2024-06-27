a026681c291c6d3c1590ca2f336af40edc85b264
Al-Amin Biswas <abjessbd@gmail.com>
2024-06-28 00:29:31 +0600

    function added

---
+++ index.js
@@ -0,0 +1,14 @@
+const a = [12, 22, 44, 56, 1, 3, 0, 10];
+const b = 44;
+
+function sum(arr) {
+    let result = 0;
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            result = result + arr[i]
+        }
+    }
+    return result;
+}
+
+console.log(sum(a));
